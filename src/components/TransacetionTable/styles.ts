5d01d69b20a80d5469a9ed3eb841eec79a7cd83e
Gabriel <gabriel_oliveira_f_sousa@outlook.com>
2022-01-24 06:26:39 -0300

    ultimos ajustes

--- src/components/TransacetionTable/styles.ts
+++ src/components/TransacetionTable/styles.ts
@@ -3,14 +3,14 @@ import styled from "styled-components";
 export const Container = styled.div`
     margin-top:4rem;
     table{
+        width:100%;
+        border-spacing:0 0.5rem;
         .deposit{ 
             color:var(--green);
         }
         .withdraw{ 
             color:var(--red);
-        }2
-        width:100%;
-        border-spacing:0 0.5rem;
+        }
         th{
             color:var(--text-body);
             font-weight:400;
